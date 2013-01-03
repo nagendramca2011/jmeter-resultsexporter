d2e2b143d6258f8c9abb3bded2ddf153668d8648
soframel <sophie@massen-ramel.net>
2013-01-03 20:36:53 +0100

    added features

--- src/main/java/org/soframel/jmeter/resultsexporter/ResultExporterListener.java
+++ src/main/java/org/soframel/jmeter/resultsexporter/ResultExporterListener.java
@@ -1,11 +1,17 @@
 package org.soframel.jmeter.resultsexporter;
 
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
@@ -39,16 +45,43 @@ import org.apache.log.Logger;
  * @author soframel
  * 
  */
-public class ResultExporterListener extends ResultCollector {
+public class ResultExporterListener extends ResultCollector implements ActionListener{
 
-	public static String EXPORT_COMMAND = "export";
+	private static AtomicBoolean exporting=new AtomicBoolean(false);
 	
 	private static final long serialVersionUID = -3408497844654656634L;
 
 	private static final Logger log = LoggingManager.getLoggerForClass(); 
 	
-	public ResultExporterListener() {
+	public final static String EXPORTNOW_COMMAND="exportNow";
+	private final static String RESOURCES_FILENAME="/resultsexporter.properties";
+	private static Properties resources;
 	
+	public final static String POPUPSBEFORETEST_PROPERY="popupsBeforeTest";
+	public final static String DOEXPORT_PROPERY="doExportNow";
+	public final static String FULLSCREENBEFOREEXPORT_PROPERTY="fullscreenBeforeExport";
+	
+	private String exportDirname;
+	private String rootFilename;
+	
+	public ResultExporterListener() {
+	}
+	
+	static{
+		loadResources();
+	}
+	
+	public static void loadResources(){
+		InputStream in=ResultExporterListener.class.getResourceAsStream(RESOURCES_FILENAME);
+		resources=new Properties();
+		try {
+			resources.load(in);
+		} catch (IOException e) {
+			log.error("IOException while loading resources file "+RESOURCES_FILENAME+": "+e.getMessage(), e);
+		}
+	}
+	public static String getResource(String key){
+		return resources.getProperty(key);
 	}
 
 	@Override
@@ -57,25 +90,29 @@ public class ResultExporterListener extends ResultCollector {
 		File f = new File(this.getFilename());
 		f.delete();
 
+		//test if configuration should be asked
+		if(this.isPopupsBeforeTest())
+			this.prepareExport();
+		
 		super.testStarted();
 	}
 
 	@Override
 	public void testEnded() {
 		super.testEnded();
-		this.exportAll();
+		this.runExport();
 	}
-
-	/**
-	 * Export all the test results
-	 */
-	public void exportAll() {		
-		log.debug("ResultExporterListener: exportAll called");
-		log.debug("ResultExporter: name="+this.getName());
-
+	
+	public void runExport(){
+		if(!this.isPopupsBeforeTest())
+			this.prepareExport();
+		if(this.getExportDirname()!=null && !this.getExportDirname().equals(""))
+			this.doExport();
+	}
+	
+	public void prepareExport(){
 		// show popup to choose if you want to save your tests
-		int result = JOptionPane.showConfirmDialog(null,
-				"Do you want to export the results for this test?", "Export?",
+		int result = JOptionPane.showConfirmDialog(null,resources.getProperty("askexport.message"), resources.getProperty("askexport.title"),
 				JOptionPane.YES_NO_OPTION);
 		if (result == JOptionPane.YES_OPTION) {
 			log.debug("ResultExporterListener: exporting");
@@ -88,8 +125,10 @@ public class ResultExporterListener extends ResultCollector {
 			File rootFile = new File(filename);
 			File rootDir = rootFile.getParentFile();
 
+			this.setRootFilename(filename);
+			
 			////Directory of test
-			String inputValue = JOptionPane.showInputDialog("Name of the test");
+			String inputValue = JOptionPane.showInputDialog(resources.getProperty("testname.message"));
 			if (inputValue == null || inputValue.equals("")
 					|| inputValue.trim().equals("")) {
 				inputValue = "default";
@@ -100,7 +139,7 @@ public class ResultExporterListener extends ResultCollector {
 			////if directory exists: ask if we continue
 			if(exportDir.exists()){
 				int delete = JOptionPane.showConfirmDialog(null,
-						"Directory exists. Delete its content?", "Directory already exists",
+						resources.getProperty("direxists.message"), resources.getProperty("direxists.title"),
 						JOptionPane.YES_NO_OPTION);
 				if(delete==JOptionPane.YES_OPTION){
 					try {
@@ -113,35 +152,67 @@ public class ResultExporterListener extends ResultCollector {
 					return;
 			}
 			
-			////create dir
-			if (!exportDir.exists())
-				exportDir.mkdirs();			
+			this.setExportDirname(exportDir.getPath());
+		}
+	}
+	
+	/**
+	 * Export all the test results
+	 */
+	private void doExport() {			
+		log.debug("ResultExporterListener: exportAll called");
+		log.debug("ResultExporter: name="+this.getName());
 
-			//// copy main CSV/XML file
-			File mainFile = new File(exportDir, rootFile.getName());
-			try {
-				FileUtils.copyFile(rootFile, mainFile);
-			} catch (IOException e) {
-				log.error("An IOException occured while copying main file: "+ e.getMessage(), e);
+		if(exporting.get())
+			log.warn("Not exporting because an export operation is currenty in progress");
+		else{
+			exporting.set(true);	
+			
+			try{
+				//change cursor
+				GuiPackage.getInstance().getMainFrame().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+				
+				//get configured directory and rootFile
+				String rootFilename=this.getRootFilename();
+				File rootFile=new File(rootFilename);
+				String exportDirname=this.getExportDirname();
+				File exportDir=new File(exportDirname);
+				
+				////create dir
+				if (!exportDir.exists())
+					exportDir.mkdirs();			
+	
+				//// copy main CSV/XML file
+				File mainFile = new File(exportDir, rootFile.getName());
+				try {
+					FileUtils.copyFile(rootFile, mainFile);
+				} catch (IOException e) {
+					log.error("An IOException occured while copying main file: "+ e.getMessage(), e);
+				}
+	
+				////Find other plugins and export images
+				//extend window to take screenshots in fullscreen
+				if(this.getPropertyAsBoolean(FULLSCREENBEFOREEXPORT_PROPERTY))
+					GuiPackage.getInstance().getMainFrame().setExtendedState(JFrame.MAXIMIZED_BOTH);
+	
+				//Find ThreadGroup from tree
+				JTree tree=GuiPackage.getInstance().getMainFrame().getTree();	
+				JMeterTreeNode tgNode=this.getThreadGroupTreeNode(tree);
+			
+				//open once every TestElement			
+				this.showAllTestElements(exportDir, tree, tgNode);
+				
+				//export images		
+				log.info("exporting nodes' images");
+				this.exportAllTestElementsAsImages(exportDir, tgNode);
+				
+				////Confirmation of end of processing
+				JOptionPane.showMessageDialog(null, resources.getProperty("exportfinished.message"), resources.getProperty("exportfinished.title"), JOptionPane.INFORMATION_MESSAGE);
+			
+			}finally{
+				exporting.set(false);
+				GuiPackage.getInstance().getMainFrame().setCursor(Cursor.getDefaultCursor());
 			}
-
-			////Find other plugins and export images
-			//extend window to take screenshots in fullscreen
-			GuiPackage.getInstance().getMainFrame().setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-			//Find ThreadGroup from tree
-			JTree tree=GuiPackage.getInstance().getMainFrame().getTree();	
-			JMeterTreeNode tgNode=this.getThreadGroupTreeNode(tree);
-		
-			//open once every TestElement			
-			this.showAllTestElements(exportDir, tree, tgNode);
-			
-			//export images		
-			log.info("exporting nodes' images");
-			this.exportAllTestElementsAsImages(exportDir, tgNode);
-			
-			////Confirmation of end of processing
-			JOptionPane.showMessageDialog(null, "Export is finished", "Export", JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
 	
@@ -165,14 +236,19 @@ public class ResultExporterListener extends ResultCollector {
 				//wait until component is shown
 				JMeterGUIComponent component = GuiPackage.getInstance().getGui(node.getTestElement());
 				JComponent jcomp=(JComponent) component;
-				while(!jcomp.isShowing()){
+				int maxIterations=20;
+				int nbIteration=0;
+				while(!jcomp.isShowing() && nbIteration<maxIterations){
 					log.info("Waiting for component to show: "+node.getTestElement().getName());
 					try {
 						Thread.sleep(100);
+						nbIteration++;
 					} catch (InterruptedException e) {
 						log.error("InterruptedException while waiting in showAllTestElements: "+e.getMessage(), e);
 					}
 				}
+				if(nbIteration==maxIterations)
+					log.warn("Waited for component until maximum wait time, screenshot will probably be empty: "+node.getTestElement().getName());
 			}
 			
 			//process children
@@ -354,5 +430,46 @@ public class ResultExporterListener extends ResultCollector {
         	}
         }
     }
+
+	public void actionPerformed(ActionEvent event) {
+		if(EXPORTNOW_COMMAND.equals(event.getActionCommand())){
+			this.runExport();
+		}	
+	}
+
+	////// getters/setters for normal fields
+	
+	public void setRootFilename(String filename){
+		this.rootFilename=filename;
+	}
+	public String getRootFilename(){
+		String p=rootFilename;
+		return p;
+	}
+	public void setExportDirname(String dirname){
+		this.exportDirname=dirname;
+	}
+	public String getExportDirname(){
+		String p=this.exportDirname;
+		return p;		
+	}
+	
+	///// getters/setters for properties
+	
+	public boolean isPopupsBeforeTest() {
+		return this.getPropertyAsBoolean(POPUPSBEFORETEST_PROPERY);
+	}
+
+	public void setPopupsBeforeTest(boolean popupsBeforeTest) {
+		this.setProperty(POPUPSBEFORETEST_PROPERY, popupsBeforeTest);
+	}
+
+	public boolean isFullscreenBeforeExport() {
+		return this.getPropertyAsBoolean(FULLSCREENBEFOREEXPORT_PROPERTY);
+	}
+
+	public void setFullscreenBeforeExport(boolean fullscreenBeforeExport) {
+		this.setProperty(FULLSCREENBEFOREEXPORT_PROPERTY, fullscreenBeforeExport);
+	}
 	
 }
--- src/main/java/org/soframel/jmeter/resultsexporter/ResultExporterVisualizer.java
+++ src/main/java/org/soframel/jmeter/resultsexporter/ResultExporterVisualizer.java
@@ -1,8 +1,16 @@
 package org.soframel.jmeter.resultsexporter;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testelement.TestElement;
@@ -20,6 +28,11 @@ public class ResultExporterVisualizer extends AbstractVisualizer{
 	 */
 	private static final long serialVersionUID = -8828414474135550223L;
 
+	
+	private JCheckBox popupsBeforeTestCheckbox;
+	private JButton exportNowButton;
+	private JCheckBox fullscreenBeforeExportCheckbox;
+	
 	public ResultExporterVisualizer(){	
 		this.init();
 	}
@@ -30,17 +43,59 @@ public class ResultExporterVisualizer extends AbstractVisualizer{
 		
 		Box box = Box.createVerticalBox();
 		box.add(makeTitlePanel());
-		add(box,BorderLayout.NORTH);	
+		box.setAlignmentX(Component.LEFT_ALIGNMENT);
+		box.setAlignmentY(Component.TOP_ALIGNMENT);
+		add(box,BorderLayout.NORTH);
+		
+		Box box1=Box.createHorizontalBox();
+		box1.setAlignmentX(Component.LEFT_ALIGNMENT);
+		box.add(box1,BorderLayout.WEST);
+		JLabel label=new JLabel(ResultExporterListener.getResource("configuration.popupsBeforeTest.label"));
+		box1.add(label);		
+		popupsBeforeTestCheckbox=new JCheckBox();
+		popupsBeforeTestCheckbox.setSelected(false);
+		box1.add(popupsBeforeTestCheckbox);		
+		
+		Box box2=Box.createHorizontalBox();
+		box2.setAlignmentX(Component.LEFT_ALIGNMENT);
+		box.add(box2,BorderLayout.WEST);
+		JLabel labelfull=new JLabel(ResultExporterListener.getResource("configuration.fullscreenBeforeExport.label"));
+		box2.add(labelfull);
+		fullscreenBeforeExportCheckbox=new JCheckBox();
+		fullscreenBeforeExportCheckbox.setSelected(false);
+		box2.add(fullscreenBeforeExportCheckbox);
+		
+		exportNowButton=new JButton(ResultExporterListener.getResource("configuration.exportNowButton.label"));
+		exportNowButton.setActionCommand(ResultExporterListener.EXPORTNOW_COMMAND);
+		exportNowButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+		box.add(exportNowButton,BorderLayout.WEST);
+		
+		box.add(Box.createVerticalGlue(), BorderLayout.SOUTH);
+		add(Box.createVerticalGlue(), BorderLayout.SOUTH);
 	}
 	
 	@Override
 	public void configure(TestElement el) {		
-		super.configure(el);	
+		super.configure(el);
+		if(el instanceof ResultExporterListener){
+			ResultExporterListener listener=(ResultExporterListener) el;
+			
+			if(exportNowButton.getActionListeners().length==0)
+				exportNowButton.addActionListener(listener);
+		
+			popupsBeforeTestCheckbox.setSelected((listener).isPopupsBeforeTest());
+			fullscreenBeforeExportCheckbox.setSelected(listener.isFullscreenBeforeExport());
+		}
 	}
 
 	@Override
 	public void modifyTestElement(TestElement el) {
 		super.modifyTestElement(el);
+		if(el instanceof ResultExporterListener){
+			ResultExporterListener listener=(ResultExporterListener) el;
+			listener.setPopupsBeforeTest(popupsBeforeTestCheckbox.isSelected());
+			listener.setFullscreenBeforeExport(fullscreenBeforeExportCheckbox.isSelected());
+		}
 	}
 	
 	@Override
